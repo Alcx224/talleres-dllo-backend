d4fb7835ebe69b0bfdbc79c24b6724ff39c209f2
Alcx224 <64863123+Alcx224@users.noreply.github.com>
2024-08-09 18:20:03 -0500

    afterExplanation

--- taller1.js
+++ taller1.js
@@ -1,1 +1,26 @@
+// Hola mundo
+
+/*
+Hola mundos
+*/
+
+let variable1 = 1;
+console.log(typeof variable1); //number (Js do not manage other concepts like int or float, its the same)
+
+const variable2 = "1";
+console.log(typeof variable2); //string,  You can use "" or ''
+//Semicolons are not mandatory in js but its good practice to use them
+//Variables are mutable 
+
+// you cannot asing new values to const and always must have something when declared
+// + can be use to concatenate
+// You can concatenate string and numbers cuz js was made in 10 days and will always convert that to string
+// In js you can use, =, ==, ===
+
+const maxFactorial = 5;
+let factorial = 1;
+
+for (let i = 1; i<= maxFactorial; 1++) {
+    
+}
 
